b13bd3778b35a74bb35534ab0a8741dac46e998e
zahramarri <zahramarri.dev@gmail.com>
2022-03-29 23:26:21 +0430

    declare six classes implementing the three interfaces

--- src/main/kotlin/hwork5-1.kt
+++ src/main/kotlin/hwork5-1.kt
@@ -6,6 +6,84 @@ interface Vehicle {
 
 interface PublicTransportationVehicle: Vehicle
 
+interface ServiceTransportationVehicle: Vehicle
+
 interface PrivateTransportationVehicle: Vehicle
 
-interface ServiceTransportationVehicle: Vehicle
+class Subway: PublicTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Taxi: PublicTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Ambulance: ServiceTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Truck: ServiceTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+
+class SportCar: PrivateTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+
+class HatchbackCar: PrivateTransportationVehicle {
+    override val name: String
+        get() = TODO("Not yet implemented")
+
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
