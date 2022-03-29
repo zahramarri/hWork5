0bb8e0b1e1a54d9e16fea5bb4074afbff5a01399
zahramarri <zahramarri.dev@gmail.com>
2022-03-30 00:21:22 +0430

    complete implementation

--- src/main/kotlin/hwork5-1.kt
+++ src/main/kotlin/hwork5-1.kt
@@ -1,7 +1,7 @@
 interface Vehicle {
     val name: String
-    fun maxSpeed()
-    fun maxCapacity()
+    val maxSpeed: Int
+    val maxCapacity: Int
 }
 
 interface PublicTransportationVehicle: Vehicle
@@ -11,79 +11,49 @@ interface ServiceTransportationVehicle: Vehicle
 interface PrivateTransportationVehicle: Vehicle
 
 class Subway: PublicTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Subway"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 28
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 500
 }
 
 class Taxi: PublicTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Taxi"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 80
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 4
 }
 
 class Ambulance: ServiceTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Ambulance"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 124
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 5
 }
 
 class Truck: ServiceTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Truck"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 85
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 3
 }
 
 class SportCar: PrivateTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Sport Car"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 400
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 2
 }
 
 class HatchbackCar: PrivateTransportationVehicle {
-    override val name: String
-        get() = TODO("Not yet implemented")
+    override val name: String = "Hatchback Car"
 
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
-    }
+    override val maxSpeed: Int = 280
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
-    }
+    override val maxCapacity: Int = 4
 }
