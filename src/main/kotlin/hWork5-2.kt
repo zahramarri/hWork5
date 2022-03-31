c634740fc8f75ca2f90230ba51416483e60b6c0d
zahramarri <zahramarri.dev@gmail.com>
2022-03-31 16:59:23 +0430

    complete implementation of the classes

--- src/main/kotlin/hWork5-2.kt
+++ src/main/kotlin/hWork5-2.kt
@@ -1,6 +1,27 @@
-abstract class Employee
-class Manager
-class Clerk
+abstract class Employee(
+    val name: String,
+    val age: Int,
+    val hourRate: Double
+) {
+
+    abstract fun salary(hours: Double): Double
+    override fun toString(): String {
+        return "name: $name, age: $age, hour rate: $hourRate"
+    }
+}
+
+class Manager(name: String, age: Int, hourRate: Double) : Employee(name, age, hourRate) {
+    override fun salary(hours: Double): Double {
+        return hourRate * hours
+    }
+}
+
+class Clerk(name: String, age: Int, hourRate: Double) : Employee(name, age, hourRate) {
+    override fun salary(hours: Double): Double {
+        return hourRate * hours / 2
+    }
+}
+
 data class Customer(val name: String, val age: Int, val gender: Gender)
 enum class Gender {
     Male, Female
