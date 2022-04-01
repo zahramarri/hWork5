56c4ec4f7dcb4ebff3f1e1a6a590942222776e05
zahramarri <zahramarri.dev@gmail.com>
2022-04-01 14:00:26 +0430

    create three objects of class ArrayList to test fun print

--- src/main/kotlin/hWork5-4.kt
+++ src/main/kotlin/hWork5-4.kt
@@ -1,3 +1,13 @@
 fun <T> ArrayList<T>.print() {
     println(this)
 }
+
+fun main() {
+    val arrayList1 = arrayListOf<Int>(1, 2, 3)
+    val arrayList2 = arrayListOf<String>("a", "aa", "aaa")
+    val arrayList3 = arrayListOf<Boolean>(true, false, true)
+
+    arrayList1.print()
+    arrayList2.print()
+    arrayList3.print()
+}
