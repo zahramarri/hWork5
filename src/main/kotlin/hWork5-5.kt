99f31d588b721d1c54d36f2af63ef4d443805801
zahramarri <zahramarri.dev@gmail.com>
2022-04-01 14:56:49 +0430

    create two objects of class List to test extension function slice

--- src/main/kotlin/hWork5-5.kt
+++ src/main/kotlin/hWork5-5.kt
@@ -8,3 +8,14 @@ fun <T> List<T>.slice(slicer: Int) {
         else -> println(this)
     }
 }
+
+fun main() {
+    val list1 = listOf(1, 2, 3, 4, 5)
+    val list2 = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g')
+
+    println("-----slice list 1-----")
+    list1.slice(2)
+
+    println("-----slice list 2-----")
+    list2.slice(3)
+}
