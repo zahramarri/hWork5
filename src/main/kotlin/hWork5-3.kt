d169ebd029c499eb0b33aa4d5f843626aa89424f
zahramarri <zahramarri.dev@gmail.com>
2022-04-08 23:17:53 +0430

    change implementation of function compare and add two other objects

--- src/main/kotlin/hWork5-3.kt
+++ src/main/kotlin/hWork5-3.kt
@@ -1,24 +1,32 @@
-class Generic<T>(private val a: T, private val b: T) {
+class Generic<T: Comparable<T>?>(private val a: T, private val b: T) {
+
+//    fun compare(): T? {
+//        if (a is Int && b is Int) {
+//            return if (a > b) {
+//                a
+//            } else if (b > a) {
+//                b
+//            } else {
+//                null
+//            }
+//        } else {
+//            a as String
+//            b as String
+//            return if (a > b) {
+//                a
+//            } else if (b > a) {
+//                b
+//            } else {
+//                null
+//            }
+//        }
+//    }
 
     fun compare(): T? {
-        if (a is Int && b is Int) {
-            return if (a > b) {
-                a
-            } else if (b > a) {
-                b
-            } else {
-                null
-            }
-        } else {
-            a as String
-            b as String
-            return if (a > b) {
-                a
-            } else if (b > a) {
-                b
-            } else {
-                null
-            }
+        return when(a?.compareTo(b)) {
+            1 -> a
+            -1 -> b
+            else -> null
         }
     }
 }
@@ -26,7 +34,11 @@ class Generic<T>(private val a: T, private val b: T) {
 fun main() {
     val test1 = Generic(4, 7)
     val test2 = Generic("a", "b")
+    val test3 = Generic(1.0, 1.1)
+    val test4 = Generic(a = false, b = true)
 
     println(test1.compare())
     println(test2.compare())
+    println(test3.compare())
+    println(test4.compare())
 }
